9d182c3b76353fc01337f67878ca3d14ec37b732
IAMCrazyCat <liuzimingjay@vip.qq.com>
2020-06-09 23:31:01 +1000

    merge

--- Our Project/Scripts.js
+++ Our Project/Scripts.js
@@ -14,10 +14,46 @@ var controls,renderer,scene,camera;
   renderer.setSize(window.innerWidth,window.innerHeight); //Set the size of the rendering window
   document.body.appendChild(renderer.domElement ); //Add the renderer to the current document
   //Instantiate a texture loader
+  //Add keyboard listener
 
   //-------------------------------------------------------------------------------------------
-  
+ 
+/*function init() {  
+  let materialArray = [];
+  let texture_ft = new THREE.TextureLoader().load( 'bg/ft.jpg');
+  let texture_bk = new THREE.TextureLoader().load( 'bg/bk.jpg');
+  let texture_up = new THREE.TextureLoader().load( 'bg/up.jpg');
+  let texture_dn = new THREE.TextureLoader().load( 'bg/dn.jpg');
+  let texture_rt = new THREE.TextureLoader().load( 'bg/rt.jpg');
+  let texture_lf = new THREE.TextureLoader().load( 'bg/lf.jpg');
+
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+  for (let i = 0; i < 6; i++)
+            materialArray[i].side = THREE.BackSide;
+          let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
+          let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+          scene.add( skybox );  
+          animate();
+}
+      
+function animate() {
+  renderer.render(scene,camera);
+  requestAnimationFrame(animate);
+}
+init(); */
 //---------------------------------------------------------------------------------------------------
+  window.addEventListener('keydown', handleKeyDown, false);
+
+  controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+  var loader = new THREE.OBJLoader ();
+
+  
 
 
 //Global variables
@@ -163,12 +199,7 @@ function handleKeyDown(event) {
 }
 
 
-//Add keyboard listener
-window.addEventListener('keydown', handleKeyDown, false);
 
-controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-var loader = new THREE.OBJLoader ();
 
 var mtlLoader = new THREE.MTLLoader(); // OBJ + MTL loader here ------------------------------------
 
@@ -185,16 +216,26 @@ mtlLoader.load('models/chair.mtl', function(materials) {
   });
 });
 
+var objLoader = new THREE.OBJLoader();
+
+  /*objLoader.load('models/main_house.obj', function(object) {
+    object.position.y += -5;
+    object.position.x += 0;
+    object.position.z += 0;
+    object.scale.set(0.1, 0.1, 0.1)
+    scene.add(object);
+  }); */
 
 
-mtlLoader.load('models/3d-model.mtl', function(materials) {
+mtlLoader.load('models/Artichoke.mtl', function(materials) {
   materials.preload();
   var objLoader = new THREE.OBJLoader();
   objLoader.setMaterials(materials);
-  objLoader.load('models/3d-model.obj', function(object) {
-    object.position.y -=10;
-    object.position.x -=10;
-    object.scale.set(0.01,0.01,0.01)
+  objLoader.load('models/Artichoke.obj', function(object) {
+    object.position.y += -5;
+    object.position.x += -40;
+    object.position.z += -30;
+    object.scale.set(0.1, 0.1, 0.1)
     scene.add(object);
   });
 });
@@ -205,14 +246,38 @@ mtlLoader.load('models/Artichoke.mtl', function(materials) {
   objLoader.setMaterials(materials);
   objLoader.load('models/Artichoke.obj', function(object) {
     object.position.y += -5;
-    object.position.x += 100;
-    object.position.z += -50;
+    object.position.x += -25;
+    object.position.z += -30;
     object.scale.set(0.1, 0.1, 0.1)
     scene.add(object);
   });
 });
 
+mtlLoader.load('models/desk.mtl', function(materials) {
+  materials.preload();
+  var objLoader = new THREE.OBJLoader();
+  objLoader.setMaterials(materials);
+  objLoader.load('models/desk.obj', function(object) {
+    object.position.y += -5;
+    object.position.x += 50;
+    object.position.z += -30;
+    object.scale.set(10, 10, 10);
+    scene.add(object);
+  });
+});
 
+mtlLoader.load('models/painting_face.mtl', function(materials) {
+  materials.preload();
+  var objLoader = new THREE.OBJLoader();
+  objLoader.setMaterials(materials);
+  objLoader.load('models/painting_face.obj', function(object) {
+    object.position.y += 5;
+    object.position.x += 40;
+    object.position.z += -42;
+    object.scale.set(0.01, 0.01, 0.01)
+    scene.add(object);
+  });
+});
 
 //final update loop
 var MyUpdateLoop = function ( )
