e8a55bd4127072b7c7932ba56f6aacb7c2096d54
SV3N77 <50192528+SV3N77@users.noreply.github.com>
2020-06-09 18:20:04 +1000

    Update Scripts.js
    added scale

--- Our Project/Scripts.js
+++ Our Project/Scripts.js
@@ -39,21 +39,22 @@ function animate() {
   requestAnimationFrame(animate);
 }
 init();*/
+//Global variables
+var controls,renderer,scene,camera;
 
-//Create the scene
-var scene = new THREE.Scene( );
-var ratio = window.innerWidth/window.innerHeight;
-
-//Create the perspective camera
-var camera = new THREE.PerspectiveCamera(45,ratio,0.1,1000);
-camera.position.set(100,15,100); //set camera position
-camera.lookAt(0,0,0); //set camera direction
-
-var renderer = new THREE.WebGLRenderer( ); //Create the WebGL renderer
-renderer.setSize(window.innerWidth,window.innerHeight); //Set the size of the rendering window
-document.body.appendChild(renderer.domElement ); //Add the renderer to the current document
-
-//Instantiate a texture loader
+ //Create the scene 
+  scene = new THREE.Scene( );
+  ratio = window.innerWidth/window.innerHeight;
+  
+  //Create the perspective camera
+  camera = new THREE.PerspectiveCamera(45,ratio,0.1,1000);
+  camera.position.set(100,15,100); //set camera position
+  camera.lookAt(0,0,0); //set camera direction
+  
+  renderer = new THREE.WebGLRenderer( ); //Create the WebGL renderer
+  renderer.setSize(window.innerWidth,window.innerHeight); //Set the size of the rendering window
+  document.body.appendChild(renderer.domElement ); //Add the renderer to the current document
+  //Instantiate a texture loader
 var loader = new THREE.TextureLoader();
 //Allow cross origin loading
 loader.crossOrigin = '';
@@ -144,6 +145,13 @@ textureToShowWall++;
 scene.add( meshWall );
 scene.add( meshWall2 );
 });
+  //Light for test purposes
+var cameralight = new THREE.PointLight( new THREE.Color(1,1,1), 1 );
+camera.add( cameralight );
+scene.add(camera);
+
+
+
 
 function handleKeyDown(event) {
   if (event.keyCode === 49) //49 == When '1' is pressed
@@ -183,15 +191,11 @@ function handleKeyDown(event) {
   }
 }
 
+
 //Add keyboard listener
 window.addEventListener('keydown', handleKeyDown, false);
 
 controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-//Light for test purposes
-var cameralight = new THREE.PointLight( new THREE.Color(1,1,1), 1 );
-camera.add( cameralight );
-scene.add(camera);
 
 var loader = new THREE.OBJLoader ();
 
@@ -199,11 +203,10 @@ loader.load
 (
   'models/chair.obj',
   function ( object ){
+    object.scale.set(10,10,10);
     scene.add(object);
   }
 );
-
-var loader = new THREE.OBJLoader ();
 
 loader.load 
 (
