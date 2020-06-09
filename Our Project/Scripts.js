de47162f1dc1b4f09419e0de1579fafcb2145261
SV3N77 <50192528+SV3N77@users.noreply.github.com>
2020-06-09 17:49:28 +1000

    Added obj loader
    added loader

--- Our Project/Scripts.js
+++ Our Project/Scripts.js
@@ -46,7 +46,7 @@ var ratio = window.innerWidth/window.innerHeight;
 
 //Create the perspective camera
 var camera = new THREE.PerspectiveCamera(45,ratio,0.1,1000);
-camera.position.set(30,15,30); //set camera position
+camera.position.set(100,15,100); //set camera position
 camera.lookAt(0,0,0); //set camera direction
 
 var renderer = new THREE.WebGLRenderer( ); //Create the WebGL renderer
@@ -93,7 +93,7 @@ var textureToShowWall = 0;
 //Instantiate the material we will be using
 var material_floor = new THREE.MeshBasicMaterial();
 //Instantiate a geometry to use
-var geometry_floor = new THREE.BoxGeometry(30,0.5,30);
+var geometry_floor = new THREE.BoxGeometry(100,0.5,100);
 // Instatiate the mesh with the geometry and material
 var meshFloor = new THREE.Mesh( geometry_floor, material_floor );
 meshFloor.position.y-=10;
@@ -115,7 +115,7 @@ scene.add( meshFloor );
 //Instantiate the material we will be using
 var material_wall = new THREE.MeshBasicMaterial();
 //Instantiate a geometry to use
-var geometry_wall = new THREE.BoxGeometry(0.5,25,30);
+var geometry_wall = new THREE.BoxGeometry(0.5,25,100);
 // Instatiate the mesh with the geometry and material
 var meshWall = new THREE.Mesh( geometry_wall, material_wall );
 meshWall.position.y+=2.25;
@@ -123,7 +123,7 @@ meshWall.position.x-=15.25;
 
 var material_wall2 = new THREE.MeshBasicMaterial();
 //Instantiate a geometry to use
-var geometry_wall2 = new THREE.BoxGeometry(30,25,0.5);
+var geometry_wall2 = new THREE.BoxGeometry(100,25,0.5);
 // Instatiate the mesh with the geometry and material
 var meshWall2 = new THREE.Mesh( geometry_wall2, material_wall2 );
 meshWall2.position.y+=2.25;
@@ -192,6 +192,28 @@ controls = new THREE.OrbitControls( camera, renderer.domElement );
 var cameralight = new THREE.PointLight( new THREE.Color(1,1,1), 1 );
 camera.add( cameralight );
 scene.add(camera);
+
+var loader = new THREE.OBJLoader ();
+
+loader.load 
+(
+  'models/chair.obj',
+  function ( object ){
+    scene.add(object);
+  }
+);
+
+var loader = new THREE.OBJLoader ();
+
+loader.load 
+(
+  'models/house.obj',
+  function ( object ){
+    object.position.y -=10;
+    object.position.x -=10;
+    scene.add(object);
+  }
+);
 
 //final update loop
 var MyUpdateLoop = function ( )
